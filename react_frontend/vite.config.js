94904ea51bfbfc8f5b625f3474c01199193b38c6
Seb <sebbooth@yahoo.ca>
2024-03-04 16:13:22 -0800

    last try for now

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 import dotenv from "dotenv";
 
 // Load environment variables from the .env file in the parent directory
-const envPath = path.resolve(__dirname, "../.env");
+const envPath = path.resolve(__dirname, "./.env");
 const envVariables = dotenv.config({ path: envPath }).parsed;
 
 // https://vitejs.dev/config/
